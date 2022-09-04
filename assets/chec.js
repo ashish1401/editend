c6e0409cdef6a7016a1e7ab81472c1a6847a205f
Ashish Chotani <ashishchotani_ec21a16_30@dtu.ac.in>
2022-09-05 02:07:31 +0530

    mic final section ..

---
+++ assets/chec.js
@@ -0,0 +1,198 @@
+const syllabus =
+    [{
+        branch: [{
+            name: 'COE',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    }, {
+
+
+        branch: [{
+            name: 'IT',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'SE',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    },
+
+    {
+        branch: [{
+            name: 'MCE',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    },
+
+    {
+        branch: [{
+            name: 'ECE',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'EE',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    },
+
+    {
+        branch: [{
+            name: 'COE',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    }
+
+    ]
+
+
