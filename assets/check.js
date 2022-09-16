bd5ad419f68618b70fcc51eb54fa6d46f86101cf
Ashish Chotani <ashishchotani_ec21a16_30@dtu.ac.in>
2022-09-17 00:36:32 +0530

    final draft1.0

---
+++ assets/check.js
@@ -0,0 +1,386 @@
+const syllabus =
+    [{
+        branch: [{
+            name: 'COE',
+            course: [{
+                name: 'AE',
+                title: 'https://docs.google.com/document/d/1h4eV3ZoV1jGWNGptqdD3ZLxluE8e0qJjttQEsBia2Vg/edit?usp=sharing  '
+            },
+            {
+                name: 'OOPs',
+                title: 'https://docs.google.com/document/d/1ppjMqptpMYC-hriX9GYRKIwbR352nDcjr0AUgJigjXU/edit?usp=sharing '
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'https://docs.google.com/document/d/1NQeu-sGxJnZeGL9BNnmd5uHz9K9V0wjYOYvm1sAEbzA/edit?usp=sharing'
+            },
+            {
+                name: 'DST',
+                title: 'https://docs.google.com/document/d/1CTTCQEUCmDHdwTUo125cKLJ7QTKPBhbOsZ2C1PG4OOY/edit?usp=sharing'
+            },
+            {
+                name: 'MNS',
+                title: 'https://docs.google.com/document/d/1p-DWVwmzO0AKeOM6NW6hb2cwz_afM97U_pBd39KakKU/edit?usp=sharing'
+            },
+            ]
+        }]
+    },
+    {
+
+
+        branch: [{
+            name: 'IT',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'SE',
+            course: [{
+                name: 'DE',
+                title: 'https://docs.google.com/document/d/1-PEzdst67L8PE7NGC0qu9J_YIKcEd0VIwyo5bBMPTkk/edit?usp=sharing'
+            },
+            {
+                name: 'OOPs',
+                title: 'https://docs.google.com/document/d/16csgRvwuaAnR8qgBQA-IhQbvErGr8cjCwehsfy7veCg/edit?usp=sharing'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'https://docs.google.com/document/d/17JV8vOCnSnuFvXdkpIy4NeX27NgBMPP1f1KN4tHjH_I/edit?usp=sharing'
+            },
+            {
+                name: 'SE',
+                title: 'https://docs.google.com/document/d/1O9SWgxFChFiLXJ-TQP1LHR7h0OfvDuDM-SPBFUK1SWU/edit?usp=sharing'
+            },
+            {
+                name: '0S',
+                title: 'https://docs.google.com/document/d/19Yd4BkAWJS7L89wvy-5nvnojnQz5ZBnZe1dXIBPIE0M/edit?usp=sharing'
+            },
+            ]
+        }]
+    },
+
+    {
+        branch: [{
+            name: 'MCE',
+            course: [{
+                name: 'MATHEMATICS-3',
+                title: 'https://docs.google.com/document/d/1f1oVc_7mx4KWDPSjQ6TjAK56bPxWnLZP5-47p0fhDi8/edit?usp=drivesdk'
+            },
+            {
+                name: 'EAD-MCE',
+                title: 'https://docs.google.com/document/d/1e3WlWmItRdA2PTwq_E7MYoB0ZO7G7SXHGegQFBDg7nY/edit?usp=drivesdk'
+            },
+
+            {
+                name: 'PNS',
+                title: 'https://docs.google.com/document/d/1paLL94vyKx_A5MTJaZZo2EizpHv48-SWuuBkoBFXetA/edit?usp=drivesdk'
+            },
+            {
+                name: 'DS',
+                title: 'https://docs.google.com/document/d/1NQeu-sGxJnZeGL9BNnmd5uHz9K9V0wjYOYvm1sAEbzA/edit?usp=sharing'
+            },
+            {
+                name: 'DM',
+                title: 'https://docs.google.com/document/d/15lg19wKlbQ8HjfEyvmxQrCVk05nxCT5VRi4ZHYOIBV0/edit?usp=drivesdk'
+            },
+            ]
+        }]
+    },
+
+    {
+        branch: [{
+            name: 'ECE',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'EE',
+            course: [{
+                name: 'NEOM',
+                title: 'https://docs.google.com/document/d/1OlqoKecGRnmCtuvtBj81lrciP3P46brzJXDWglgbncQ/edit?usp=sharing'
+            },
+            {
+                name: 'EDC',
+                title: 'https://docs.google.com/document/d/1Hq5wICP_sdAilSRkZGsdSHbXqL2Tx5-vx391Hd06Ivo/edit?usp=sharing'
+            },
+                ,
+            {
+                name: 'EECT',
+                title: 'https://docs.google.com/document/d/1-o7fGIgiTqKEK5jzb2mv0zjV7G_c1cThT45nnCFHUeo/edit?usp=sharing'
+            },
+            {
+                name: 'NAS',
+                title: 'https://docs.google.com/document/d/1C6R8N8ARvLlS_MjlML66EE-KqWlzFaFm0moH-oJLtto/edit?usp=sharing'
+            },
+            {
+                name: 'EMFT',
+                title: 'https://docs.google.com/document/d/1xRBWEuEJXmETRkpj9gNPRfPkTyBANw1znvKPnchCVpo/edit?usp=sharing'
+            },
+            ]
+        }]
+    },
+
+    {
+        branch: [{
+            name: 'EP',
+            course: [{
+                name: 'EM',
+                title: 'https://docs.google.com/document/d/1VLECWOYWf-Vs5hC7oaeH5ypxiPpyimxvfpbShKUacak/edit?usp=drivesdk'
+            },
+            {
+                name: 'IC',
+                title: 'https://docs.google.com/document/d/10WJnwSb7jqTrDrha_ZhxokMI5PIUeG4hGbKLHJKXw-Q/edit?usp=drivesdk'
+            },
+                ,
+            {
+                name: 'MP',
+                title: 'https://docs.google.com/document/d/1DpBnydBBygt-ligtMlMnRLCacHpXd7BkL0-fSO83vNk/edit?usp=drivesdk'
+            },
+            {
+                name: 'CQM',
+                title: 'https://docs.google.com/document/d/1-zP6xybYmYsakEaS_i84MFWA4wKz40yfk38CkcZLFqE/edit?usp=drivesdk'
+            },
+            {
+                name: 'DE',
+                title: 'https://docs.google.com/document/d/1nlQlVX-hN-RuuYcK8FMv67q37bopeWy9okhpqFQ_hJ0/edit?usp=drivesdk'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'ME',
+            course: [{
+                name: 'MOS',
+                title: 'https://docs.google.com/document/d/1QpkvNdtgSOsFrLOFhK3YDpZqzQQxTTELkV-HzWulqZ4/edit?usp=sharing'
+            },
+            {
+                name: 'EMM',
+                title: 'https://docs.google.com/document/d/14-3vSQqTR0xlvuLD6OJcvdECg9ejLWSjlr3oqyeHqec/edit?usp=sharing'
+            },
+                ,
+            {
+                name: 'EAD',
+                title: 'https://docs.google.com/document/d/1MYEFIxWTA2LiZ1BqaNeMg7iJkqz8V8uS21sAcDHYbj0/edit?usp=sharing'
+            },
+            {
+                name: 'MD-SM',
+                title: 'https://docs.google.com/document/d/1ElF4IRQ8n-wjp1uIxOoO0tUSKsqHn-uQafEYFlWlAQg/edit?usp=sharing'
+            },
+            {
+                name: 'TE 1',
+                title: 'https://docs.google.com/document/d/1NcbCoLhVBlbgmR11ytKCilrX4JlnIFbnFQYeUZmwtnw/edit?usp=sharing'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'MAM',
+            course: [{
+                name: 'Quantitative Techniques',
+                title: 'https://docs.google.com/document/d/1kACRrKZr7f4a9w02iQlcKMjs6BPCvJYHKASK60qbS3g/edit?usp=sharing'
+            },
+            {
+                name: 'Engineering Mechanics',
+                title: 'https://docs.google.com/document/d/1aAQckNl2VKlkvZS7LEDHfNO1fey6MMMhHAiIO2l8_e8/edit?usp=sharing'
+            },
+                ,
+            {
+                name: 'Thermodynamics',
+                title: 'https://docs.google.com/document/d/1xu29tT_6ZQ4q-qPqR--5lR9iLimUrqXbd4XlOpdjbeM/edit?usp=sharing'
+            },
+            {
+                name: 'Manufacturing Machines',
+                title: 'https://docs.google.com/document/d/1oxXEs6BtueSzvNeBeURpL7dHTlz2rSTwmvjiaoXwGq4/edit?usp=sharing'
+            },
+            {
+                name: 'EAD',
+                title: 'https://docs.google.com/document/d/1ILqvoSLBqCDB6z0sn9kfF8Fi1i-z9kuMtOerdPYXNMI/edit?usp=sharing'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'CE',
+            course: [{
+                name: 'CEBA',
+                title: 'https://docs.google.com/document/d/1fWa4ARTM2e91E2T-LC5YZHA_NQEeXSZ1fEPcxPxc1YY/edit?usp=sharing'
+            },
+            {
+                name: 'EM',
+                title: 'https://docs.google.com/document/d/1LpbjvZ7iFHg-owDq18ek4bZM43pZcdQdcl0DlylBIgI/edit?usp=sharing'
+            },
+                ,
+            {
+                name: 'FM',
+                title: 'https://docs.google.com/document/d/1sHs35Q9dKJHOqwXfbfa1b7D7Y-UHB4gQNfoxF8AzlQo/edit?usp=sharing'
+            },
+            {
+                name: 'EAD-CE',
+                title: 'https://docs.google.com/document/d/1PZEHd4mDKctXXq0Msq5L34CgTbQTX1KhOUtMo4Ima7o/edit?usp=sharing'
+            },
+            {
+                name: 'BEI',
+                title: 'https://docs.google.com/document/d/12lIU0R838Zfoszf3nAlBp3kNodA319QUF4xSm-aBtZM/edit?usp=sharing'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'CHE',
+            course: [{
+                name: 'AE',
+                title: 'google.com'
+            },
+            {
+                name: 'OOPs',
+                title: 'google.com'
+            },
+                ,
+            {
+                name: 'DS',
+                title: 'google.com'
+            },
+            {
+                name: 'DST',
+                title: 'google.com'
+            },
+            {
+                name: 'MNS',
+                title: 'google.com'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'PIE',
+            course: [{
+                name: 'MM',
+                title: 'https://docs.google.com/document/d/1khqA--MT_jOktdzc6omdMCPTsSnNSzNVaIgJDnM0rdc/edit?usp=sharing'
+            },
+            {
+                name: 'EAD',
+                title: 'https://docs.google.com/document/d/1_YzNsnHX9HqC_NnXNMQZcPGXPBr0jFNJcpnF_GF4nDc/edit?usp=sharing'
+            },
+                ,
+            {
+                name: 'KDOM',
+                title: 'https://docs.google.com/document/d/1qjaFgnn2XV1KaPJ3BxGQAiLJIHEW98k9S9rCml4b86w/edit?usp=sharing'
+            },
+            {
+                name: 'EMM',
+                title: 'https://docs.google.com/document/d/14-3vSQqTR0xlvuLD6OJcvdECg9ejLWSjlr3oqyeHqec/edit?usp=sharing'
+            },
+            {
+                name: 'TE',
+                title: 'https://docs.google.com/document/d/1NcbCoLhVBlbgmR11ytKCilrX4JlnIFbnFQYeUZmwtnw/edit?usp=sharing'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'BT',
+            course: [{
+                name: 'AM',
+                title: 'https://docs.google.com/document/d/1Yhq18Ggy3mil0oAZnnbJesXcedwtRhqsUp5ppJTFmjo/edit?usp=sharing'
+            },
+            {
+                name: 'IB',
+                title: 'https://docs.google.com/document/d/1RVSHnydrXjqNr3DtxcAsS8_ZNmdxBFR4-OqLUzle6N4/edit?usp=sharing'
+            },
+                ,
+            {
+                name: 'Biochemistry',
+                title: 'https://docs.google.com/document/d/12BoYpynErC-EC6xkHcJce8Vtst92hZW9__LBto2MDxA/edit?usp=sharing'
+            },
+            {
+                name: 'FCB',
+                title: 'https://docs.google.com/document/d/1CJEoZx7yJMlBhUgcrtlqe7sPpTVyKPUUUnNejU9kyVY/edit?usp=sharing'
+            },
+            {
+                name: 'Genetics',
+                title: 'https://docs.google.com/document/d/1F-5HxAx-x9XbKJdT4OoNu6iqqxhQngWXLwfzSm_xYUU/edit?usp=sharing'
+            },
+            ]
+        }]
+    },
+    {
+        branch: [{
+            name: 'ENE',
+            course: [{
+                name: 'CE251',
+                title: 'https://docs.google.com/document/d/1QBlhPY-aIfsD8oxFZzv54AYtz3SZdZi9qXc3u0kEnm0/edit?usp=sharing'
+            },
+            {
+                name: 'EN201',
+                title: 'https://docs.google.com/document/d/11g1DKei9B5KsCwMH82cYzwcyB7nXX0K52kqz3eeQJZ8/edit?usp=sharing'
+            },
+                ,
+            {
+                name: 'EN207',
+                title: 'https://docs.google.com/document/d/11lcwn-gApv6J1xhkiYRFmXmWlD95A7DrWAauhJ4TK28/edit?usp=sharing'
+            },
+            {
+                name: 'EN203',
+                title: 'https://docs.google.com/document/d/1pZNnjHK1yDvt2UCk40kzLFvYE3H5ARVZxkMRbCcFNHg/edit?usp=sharing'
+            },
+            {
+                name: 'EN205',
+                title: 'https://docs.google.com/document/d/1W6nC1u3qXSLqRCVEaFgQ4ik6Qx9oZS1-iT_ML7goXJY/edit?usp=sharing'
+            },
+            ]
+        }]
+    }]
+
+
